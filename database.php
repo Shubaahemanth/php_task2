6f0b9733598a179ffff9981192f19c30bc1a9aa1
Shubaahemanth <166139791+Shubaahemanth@users.noreply.github.com>
2024-04-05 23:46:19 +0530

    Update1 database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 $n=$_POST['stname'];
 $c=$_POST['stclass'];
-$con=mysqli_connect("localhost","root"," ","mydata");
-$sql="INSERT INTO data(name,class) values('$n','$c')";
+$con=mysqli_connect("phpapp-db.cxowmomm6bcy.ap-south-1.rds.amazonaws.com","phpdbuser","phpadmin","studentdb");
+$sql="INSERT INTO student(name,class) values('$n','$c')";
 $r=mysqli_query($con,$sql);
 if($r)
 {
